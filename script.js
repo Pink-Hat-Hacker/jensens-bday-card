3f611c66808a8695b33dd80f6a51e8f43645c87f
Zoe Valladares <zvalladares@wlapcs.org>
2017-03-20 15:41:15 +0000

    made audio work

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ document.getElementById("orb").addEventListener("click", function(){
 
 function changeToNight(){
     if(document.getElementById("orb").className == "sun"){
-        
+        audio.play();
         document.getElementById("sky").className = "night";
         document.getElementById("orb").className = "moon";
-        document.getElementById("moonspot1").className ="visible";
-        document.getElementById("moonspot2").className ="visible";
-        document.getElementById("moonspot3").className ="visible";
-        audio.play();
         
     }
     else if(document.getElementById("orb").className == "moon"){
         
         document.getElementById("sky").className = "day";
         document.getElementById("orb").className = "sun";
-        document.getElementById("moonspot1").className ="";
-        document.getElementById("moonspot2").className ="";
-        document.getElementById("moonspot3").className ="";
         audio.pause();
     }
 }
+/*
+<!--THIS IS A BDAY CARD FOR A FRIEND BECAUSE THEY AREN'T 
+HERE. I CAN'T FIGURE OUT HOW TO MAKE THE SONG PLAY.
+I THINK IT MIGHT BE BECAUSE IM CLICKING THE IMAGE -- NOT 
+THE SUN OR MOON. BUT THOSE ARE STILL THE CLASSES. SO IM NOT
+SURE-->*/
